7fef9ae5f00e89b4b7c337df59f8b45f19d57d6b
Louis Dureuil <ldureuil@tetrane.com>
2020-10-14 11:58:40 +0200

    Add BlockWriter

---
+++ include/common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace reven {
+namespace block {
+
+//! Mode of execution
+enum class ExecutionMode : std::uint8_t {
+	x86_64_bits = 0,
+	x86_32_bits = 1,
+};
+
+
+constexpr const char* format_version = "1.0.0";
+constexpr const char* writer_version = "1.0.0";
+
+}} // namespace reven::block
